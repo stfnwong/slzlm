a893f236373c2d4a1a82c944c3fb819281168072
Stefan Wong <stfnwong@gmail.com>
2023-07-18 14:45:06 +0100

    Some tests

--- src/comp.cpp
+++ src/comp.cpp
@@ -15,10 +15,6 @@
 
 LZWDict::LZWDict() : cur_key(0), root(std::make_unique<LZWNode>())
 {
-    // we may need to do something here 
-    //for(uint32_t i = 0; i < ALPHA_SIZE; ++i)
-    //    this->trie.insert(std::to_string(i));
-
     for(uint32_t code = 0; code < ALPHA_SIZE; ++code)
         this->insert(lzw_symbol_t(code), this->root.get());
 }
@@ -31,19 +27,12 @@ LZWNode* LZWDict::insert(const lzw_symbol_t c, LZWNode* node)
     auto& children = node->children;
     auto it = children.find(c);
     
-    //std::cout << "[" << __func__ << "] inserting [" << c << "] at node [" << node->value << "]";
-    std::cout << "[" << __func__ << "] ";
     if(it == children.end())
-    {
-        std::cout << "inserting [" << c << "] -> [" << node->value << "]";
         it = children.emplace(c, std::make_unique<LZWNode>()).first;
-    }
 
     node = it->second.get();
     node->value = this->cur_key;
     node->leaf = true;
-    std::cout << " returning node [" << node->value << "]" << std::endl;
-    // TODO: debug only, remove 
     this->cur_key++;
 
     return node;
@@ -81,39 +70,25 @@ bool LZWDict::contains(const std::string_view data) const
 }
 
 
-// TODO: remove all print statements
+// TODO: this may turn into decode....? 
 std::vector<uint16_t> LZWDict::get_code(const std::string_view word) const
 {
     std::vector<uint16_t> out;
     auto* node = this->root.get();
 
-    std::cout << "[" << __func__ << "] input symbol [" << word << "]" << std::endl;
     for(auto const c: word)
     {
-        std::cout << " checking [" << c << "]... ";
-
         auto& children = node->children;
         auto it = children.find(lzw_symbol_t(c));
         if(it == children.end())
         {
-            std::cout << " no child with symbol [" << c << "]";
             out.push_back(node->value);
             node = this->root->children.find(c)->second.get();
-            //node = this->root.get();
         }
         else
-        {
-            std::cout << " have prefix for [" << c << "]";
             node = it->second.get();
-        }
-        std::cout << std::endl;
-        std::cout << "end of loop for [" << c << "], node is [" << node->value << "] "
-            << (node->leaf ? "leaf" : "non-leaf") << std::endl;
-
     }
-    //out.push_back(node->value);
-    //if(out.size() == 0 && node != nullptr && node->leaf)
-    //if(node != this->root.get())
+
     if(node->leaf)
         out.push_back(node->value);
 
@@ -130,12 +105,11 @@ std::vector<uint32_t> LZWDict::encode(const std::string_view data)
     for(auto const c : data)
     {
         auto* result_node = this->search_node(lzw_symbol_t(c), node);
-        // TODO: lookahead isn't working correctly...
         if(result_node == nullptr)
         {
             out.push_back(node->value);
             this->insert(lzw_symbol_t(c), node);
-            node = this->root->children.find(c)->second.get();  // p = c
+            node = this->root->children.find(c)->second.get();  // p = c node
             //node = this->insert(lzw_symbol_t(c), node);  // this line makes a very deep tree
         }
         else
--- test/test_lzwdict.cpp
+++ test/test_lzwdict.cpp
@@ -10,75 +10,42 @@
 #include "comp.hpp"
 
 
-TEST_CASE("test_encode", "lzw")
-{
-    LZWDict lzw;
-
-    //const std::string input_text = "fuck your couch charlie murphy";
-    const std::string input_text = "babaabaaa";
-
-    std::vector<uint32_t> out = lzw.encode(input_text);
-
-    std::cout << "input len: " << input_text.size() << ", out len: " << out.size() << std::endl;
-
-    for(unsigned i = 0; i < out.size(); ++i)
-        std::cout << "[" << i << "]: " << out[i] << std::endl;
-
-    std::vector<std::string> search_test = {
-        "ba",
-        "ab",
-        "baa"
-    };
-
-    //std::vector<std::string> search_test = {
-    //    "fuck",
-    //    "your",
-    //    "couch",
-    //    "norm macdonald"
-    //};
-
-    for(const auto& s : search_test)
-        std::cout << "[" << s << "] " << (lzw.contains(s) ? " found" : "not found") << std::endl;
-
-}
-
-TEST_CASE("test_insert_same_string", "lzw")
+TEST_CASE("test_encode_string_1", "lzw")
 {
     LZWDict lzw;
     const std::string test_input = "aaaaab";
     auto result = lzw.encode(test_input);
 
-    std::vector<std::string> search = {
+    std::vector<std::string> words = {
         "a",
+        "b",
         "aa",
         "aaa",
-        "b"
+        "baa",
     };
 
     // {inp, code}
-    //std::vector<std::pair<std::string, uint32_t>> str_code_pairs = {
-    //    {"a", 97},
-    //    {"b", 98},
-    //    {"aa", 256},
-    //    {"aaa", 257},
-    //    {"baa", 258},
-    //};
+    using pair_t = std::pair<std::string, std::vector<lzw_symbol_t>>;
+    std::vector<pair_t> str_code_pairs = {
+        {"a", {97}},
+        {"b", {98}},
+        {"aa", {256}},
+        {"aaa", {257}},
+        {"baa", {98, 256}},
+    };
 
-    //for(const auto& p: str_code_pairs)
-    //    REQUIRE(lzw.get_code(p.first) == p.second);
+    for(unsigned w = 0; w < words.size(); ++w)
+    {
+        auto codes = lzw.get_code(str_code_pairs[w].first);
+        REQUIRE(codes.size() == str_code_pairs[w].second.size());
 
-    //for(const auto& s: search)
-    //    std::cout << "[" << s << "]: " << lzw.get_code(s) << std::endl;
-
-    //std::cout << "Encoding results:" << std::endl;
-    //for(unsigned i = 0; i < result.size(); ++i)
-    //    std::cout << result[i] << " ";
-    //std::cout << std::endl;
-
+        for(unsigned i = 0; i < codes.size(); ++i)
+            REQUIRE(codes[i] == str_code_pairs[w].second[i]);
+    }
 }
 
 
-TEST_CASE("test_reverse_char", "lzw")
+TEST_CASE("test_encode_string_2", "lzw")
 {
     LZWDict lzw;
 
@@ -102,13 +69,29 @@ TEST_CASE("test_reverse_char", "lzw")
         "babaa", // 256, 258
     };
 
-    for(auto const& s: words)
+    std::vector<std::vector<lzw_symbol_t>> exp_codes = {
+        {97},       // a
+        {98},       // b
+        {256},      // ba 
+        {257},      // ab
+        {258},      // baa
+        {259},      // aba
+        {260},      // aa
+        {98, 98},   // bb
+        {256, 258}, // babaa
+    };
+
+    REQUIRE(words.size() == exp_codes.size());
+
+    for(unsigned w = 0; w < words.size(); ++w)
     {
-        auto codes = lzw.get_code(s);
-        std::cout << "Codes [" << s << "]: ";
+        auto codes = lzw.get_code(words[w]);
+        //std::cout << "Codes [" << s << "]: ";
+        REQUIRE(codes.size() == exp_codes[w].size());
         for(unsigned i = 0; i < codes.size(); ++i)
-            std::cout << codes[i] << " ";
-        std::cout << std::endl;
+            REQUIRE(codes[i] == exp_codes[w][i]);
+            //std::cout << codes[i] << " ";
+        //std::cout << std::endl;
     }
 
 }
@@ -116,25 +99,25 @@ TEST_CASE("test_reverse_char", "lzw")
 
 
 // Mainly for debugging
-TEST_CASE("test_encode_method", "lzw")
-{
-    LZWDict lzw;
-
-    std::string test_input = "babaabaaa";
-    lzw.encode(test_input);
-
-    auto traversals = lzw.find_all();
-    std::cout << "Found " << traversals.size() << " traversals" 
-        << " for input [" << test_input << "]" << std::endl;
-
-    for(unsigned t = 0; t < traversals.size(); ++t)
-    {
-        if(traversals[t].size() == 1)
-            continue;
-
-        std::cout << "Traversal " << t << ": [" ;
-        for(unsigned n = 0; n < traversals[t].size(); ++n)
-            std::cout << traversals[t][n] << " ";
-        std::cout << "]" << std::endl;
-    }
-}
+//TEST_CASE("test_encode_method", "lzw")
+//{
+//    LZWDict lzw;
+//
+//    std::string test_input = "babaabaaa";
+//    lzw.encode(test_input);
+//
+//    auto traversals = lzw.find_all();
+//    std::cout << "Found " << traversals.size() << " traversals" 
+//        << " for input [" << test_input << "]" << std::endl;
+//
+//    for(unsigned t = 0; t < traversals.size(); ++t)
+//    {
+//        if(traversals[t].size() == 1)
+//            continue;
+//
+//        std::cout << "Traversal " << t << ": [" ;
+//        for(unsigned n = 0; n < traversals[t].size(); ++n)
+//            std::cout << traversals[t][n] << " ";
+//        std::cout << "]" << std::endl;
+//    }
+//}
