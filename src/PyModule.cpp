53ba9dfdb1cce9c74edde680185cc2b5266085b4
Stefan Wong <stfnwong@gmail.com>
2023-07-20 17:08:16 +0100

    This does what I think I want

--- src/PyModule.cpp
+++ src/PyModule.cpp
@@ -3,9 +3,18 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+// Stringstream test 
+#include <sstream>
+
+
 #include "Trie.hpp"
 #include "LZW.hpp"
 
+
+// Test how to pass bytes
+struct BytesModule
+{
+};
 
 
 namespace py = pybind11;
@@ -23,4 +32,17 @@ PYBIND11_MODULE(slz, m)
         .def(py::init<>())
         .def("insert", &Trie::insert)
         .def("search", &Trie::search);
+
+    m.def("return_bytes",
+            []() {
+            std::ostringstream oss;
+
+            unsigned max_alpha_size = 256;
+            for(unsigned i = 0; i < max_alpha_size; ++i)
+                oss.write(reinterpret_cast<const char*>(&i), sizeof(uint8_t));
+
+            //std::string s("\xba\xd0\xba\xd0");      // not valid UTF-8
+            return py::bytes(oss.str());        // this ends up being bytes type in python shell
+            }
+         );
 }
