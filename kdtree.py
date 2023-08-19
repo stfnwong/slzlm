16a9975da72456ab82d7a10e8887e2074a1cbbbf
Stefan Wong <stfnwong@gmail.com>
2023-08-19 10:41:24 +0100

    Terminating condition

--- kdtree.py
+++ kdtree.py
@@ -35,6 +35,9 @@ class Node:
     def dim(self) -> int:
         return self.split.dim()
 
+    def __repr__(self) -> str:
+        return f"Node({self.split})"
+
     def __str__(self) -> str:
         s = "Node "
         cur_level = 0
@@ -47,7 +50,7 @@ class Node:
                 s += "\n"
                 cur_level = node_level
 
-            s += f"{cur_node.split}<{cur_node.cut_dim}> "
+            s += f"{cur_node.split}[{cur_node.cut_dim}] "
             if cur_node.left:
                 q.append((cur_node.left, node_level+1))
             if cur_node.right:
@@ -63,10 +66,6 @@ def build_test(points: List[Point], idx_from: int, idx_to: int, depth: int) -> N
     median = idx_from + ((idx_to - idx_from) // 2)
     count = idx_to - idx_from + 1
 
-    # If there is only one point then return a leaf node
-    if count == 1:
-        return Node(points[0], axis, None, None)
-
     #pp = sorted(points[idx_from:], key=lambda p: p.coords[0])
     pp = sorted(points, key=lambda p: p.coords[0])
 
@@ -74,8 +73,12 @@ def build_test(points: List[Point], idx_from: int, idx_to: int, depth: int) -> N
 
     print(f"pp: {pp}")
 
-    midpoint = points[median]
+    midpoint = pp[median]
     node = Node(midpoint, axis, None, None)
+
+    if count == 1:
+        return node
+
     node.left = build_test(pp, idx_from, median, depth+1)
     node.right = build_test(pp, median+1, idx_to, depth+1)
 
@@ -85,7 +88,13 @@ def build_test(points: List[Point], idx_from: int, idx_to: int, depth: int) -> N
 
 def build_pre_sort(points: List[Point]) -> Node:
 
-    def build(dim_sorted: List[List[int]], points: List[Point], idx_from: int, idx_to: int, depth: int) -> Node:
+    def build(
+        dim_sorted: List[List[int]],
+        points: List[Point],
+        idx_from: int,
+        idx_to: int,
+        depth: int
+    ) -> Node:
         """
         dim_sorted: list of lists, where each inner list is the index of the sorted values
         for a given axis. For instance, set of 2-dimensional points will contain two lists
@@ -94,17 +103,21 @@ def build_pre_sort(points: List[Point]) -> Node:
         """
 
         axis = depth % points[0].dim()
-        median = idx_from - ((idx_to - idx_from) // 2)
+        median = idx_from + ((idx_to - idx_from) // 2)
         count = idx_to - idx_from
-
-
         midpoint = points[dim_sorted[axis][median]]
+
+        print(f"median: {median}, count: {count}, idx_from: {idx_from}, idx_to: {idx_to}")
+        print(f"midpoint: {midpoint}")
+
         node = Node(midpoint, axis, None, None)
+        if count <= 1:
+            return node
+
         node.left = build(dim_sorted, points, idx_from, median, depth+1)
         node.right = build(dim_sorted, points, median+1, idx_to, depth+1)
 
         return node
-
 
 
     dim_sorted = [
@@ -143,24 +156,33 @@ class KDTree:
         self.nodes.append(node)
 
 
+
 if __name__ == "__main__":
 
-    coords = [
-        (3, 1), (4, 2), (1, 3), (3, 3),
-        (5, 7), (7, 8), (1, 9), (8, 2),
-    ]
+    #coords = [
+    #    (3, 1), (4, 2), (1, 3), (3, 3),
+    #    (5, 7), (7, 8), (1, 9), (8, 2),
+    #]
+
+    #coords = [(4, 4), (2, 7), (1,6), (5, 5)]
+    coords = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6)]
+    #coords = [(7, 4), (5, 8), (3, 6), (5, 5), (2, 3), (1, 1), (6, 6), (7, 6)]
+    #coords = [(10, 10), (5, 5), (1,1)]
 
     points = [Point((x[0], x[1])) for x in coords]
     nn = build_test(points, 0, len(points)-1, 0)
-    print(nn)
+    print(f"\n{nn}\n")
+
+    nn = build_pre_sort(points)
+    print(f"\n{nn}\n")
 
 
-    from random import random
-    num_points = 4
-    xmax = 120; ymax = 119
-    points = [Point((random() * xmax, random() * ymax)) for _ in range(num_points)]
+    #from random import random
+    #num_points = 4
+    #xmax = 120; ymax = 119
+    #points = [Point((random() * xmax, random() * ymax)) for _ in range(num_points)]
 
-    #from pudb import set_trace; set_trace()
-    nn = build_test(points, 0, len(points)-1, 0)
+    ##from pudb import set_trace; set_trace()
+    #nn = build_test(points, 0, len(points)-1, 0)
 
-    print(nn)
+    #print(nn)
